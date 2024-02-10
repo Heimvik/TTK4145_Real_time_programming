a42906d2b3af82bd0e55e54a0f8c3bfe60ce5db5
heimsnik <heimsnik@Heimviks-HP-x360>
2024-02-10 19:58:46 +0100

    Made distrubution algoritm

--- main/main.go
+++ main/main.go
@@ -7,8 +7,26 @@ import (
 
 // Main file of a node
 func main() {
-	fmt.Printf("Hallo world\n")
-	var testNode node.Node //Note the syntax for access to other packages
-	testNode.Priority = 1
-	fmt.Println(testNode.Priority)
+	var elevators []*node.T_Elevator
+	for i := 0; i <= 2; i++ {
+		newElevator := node.T_Elevator{
+			Floor:     4 - i,
+			Direction: node.Down,
+			Avalibale: true,
+		}
+		if i == 2 {
+			newElevator.Avalibale = false
+		}
+		elevators = append(elevators, &newElevator)
+	}
+
+	request := node.T_Request{
+		Calltype:   node.Hall,
+		P_Elevator: elevators[0],
+		Floor:      1, //elevators[0].Floor,
+		Direction:  node.Up,
+	}
+
+	fmt.Println(node.F_AssignRequest(&request, elevators).Floor)
+
 }
