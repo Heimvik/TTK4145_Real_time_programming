c0f640b10ab50f54256a92f0c5d10a487a62c9aa
heimsnik <cmheimvik@gmail.com>
2024-02-18 02:40:21 +0100

    Imporivemnts to elevator structs

--- elevator/elevator.go
+++ elevator/elevator.go
@@ -4,15 +4,15 @@ type T_ElevatorState int
 type T_ElevatorDirection int
 
 const (
-	StateIdle     T_ElevatorState = iota
-	StateDoorOpen T_ElevatorState = iota
-	StateMoving   T_ElevatorState = iota
+	IDLE     T_ElevatorState = iota
+	DOOROPEN T_ElevatorState = iota
+	MOVING   T_ElevatorState = iota
 )
 
 const (
-	DirectionUp   T_ElevatorDirection = iota
-	DirectionDown T_ElevatorDirection = iota
-	DirectionNone T_ElevatorDirection = iota
+	UP   T_ElevatorDirection = iota
+	DOWN T_ElevatorDirection = iota
+	NONE T_ElevatorDirection = iota
 )
 
 //Keeping this in case of future improvements regarding secondary requirements,
@@ -21,9 +21,9 @@ const (
 type T_Elevator struct {
 	P_info              *T_ElevatorInfo     //Poitner to the info of this elevator
 	P_serveRequest      *T_Request          //Pointer to the current request you are serviceing
-	C_ReceiveRequest    chan T_Request      //Request to put in ServeRequest and do, you will get this from node
-	C_DistributeRequest chan T_Request      //Requests to distriburte to node, you shoud provide this to node
-	C_DistributeInfo    chan T_ElevatorInfo //Info on elevator whereabouts, you should provide this to node
+	C_receiveRequest    chan T_Request      //Request to put in ServeRequest and do, you will get this from node
+	C_distributeRequest chan T_Request      //Requests to distriburte to node, you shoud provide this to node
+	C_distributeInfo    chan T_ElevatorInfo //Info on elevator whereabouts, you should provide this to node
 	//three last ones has to be channels as elevator and node has seperate goroutines, has to be like this
 	//on comilation of one request: redistribute it on D
 }
