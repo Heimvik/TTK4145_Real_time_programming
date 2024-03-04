367709e4f088bd55879bf8caca128de7353081e4
Jonas The Blaster Brandt <jonas@brandt>
2024-03-04 20:24:29 +0100

    La til variabel synkronisering, endret timer.go, og fikset div i elevator.go og fsm.go

--- node/elevator/fsm.go
+++ node/elevator/fsm.go
@@ -1,59 +1,37 @@
 package elevator
 
-func F_fsmFloorArrival(newFloor int8) {
-	Elevator.P_info.Floor = newFloor
+func F_fsmFloorArrival(newFloor int8, elevator T_Elevator) T_Elevator{
+	elevator.P_info.Floor = newFloor
 	SetFloorIndicator(int(newFloor))
 
-	switch Elevator.P_info.State {
+	switch elevator.P_info.State {
 	case MOVING:
-		if F_shouldStop(Elevator) {
-			//make timer logic so door stays open for as long as it should
-			F_clearRequest(Elevator)
+		if F_shouldStop(elevator) {
+			elevator = F_clearRequest(elevator)
 		}
-	// case DOOROPEN:
-	// 	//make timer logic so door stays open for as long as it should
-	// 	time.Sleep(3 * time.Second) //placeholder
-	// 	Elevator.P_info.State = IDLE
-	// 	F_clearRequest(Elevator)
-
 	case IDLE:
 		SetMotorDirection(MD_Stop)
 	}
+	return elevator
 }
 
-func F_fsmObstructionSwitch(obstructed bool) {
-	switch Elevator.P_info.State {
-	case DOOROPEN:
-		if obstructed == false {
-			Elevator.P_info.State = IDLE
-		}
-	}
-}
-
-func F_fsmDoorTimeout() {
-	switch Elevator.P_info.State {
-	case DOOROPEN:
-		if C_obstruction == false {
-			Elevator.P_info.State = IDLE
-		}
-	}
-}
-
-func F_ReceiveRequest(req T_Request) {
-	switch Elevator.P_info.State {
+func F_ReceiveRequest(req T_Request, elevator T_Elevator) T_Elevator{
+	switch elevator.P_info.State {
 	case IDLE:
-		Elevator.P_serveRequest = &req
-		F_chooseDirection(Elevator)
+		elevator.P_serveRequest = &req
+		//spør heimvik om request sin state skal endres til active
+		elevator = F_chooseDirection(elevator)
 	}
+	return elevator
 }
 
-func F_sendRequest(button ButtonEvent, requestOut chan T_Request) {
-	Elevator.CurrentID++
+func F_sendRequest(button ButtonEvent, requestOut chan T_Request, elevator T_Elevator) {
+	elevator.CurrentID++
 	if button.Button == BT_Cab {
-		requestOut <- T_Request{Id: uint16(Elevator.CurrentID), State: 0, Calltype: CAB, Floor: int8(button.Floor)}
+		requestOut <- T_Request{Id: uint16(elevator.CurrentID), State: 0, Calltype: CAB, Floor: int8(button.Floor)}
 		return
 	} else {
-		requestOut <- T_Request{Id: uint16(Elevator.CurrentID), State: 0, Calltype: HALL, Floor: int8(button.Floor)}
+		requestOut <- T_Request{Id: uint16(elevator.CurrentID), State: 0, Calltype: HALL, Floor: int8(button.Floor)}
 		return
 	}
 }
--- node/elevator/run_elevator.go
+++ node/elevator/run_elevator.go
@@ -1,105 +1,85 @@
 package elevator
 
-import (
-	"fmt"
-)
+
 
 /*
 29.02.2024
 TODO:
-- Ferdigutvikle timer.go, legge til channels som sier når timer skal starte. Opprette global variabel som sier hvor lenge døra skal være åpen.
-  På denne måten kan man i fsmObstructionSwitch se om tiden døra skal være åpen har gått ut, og lukke døra hvis det ikke er noen hindring. Istedetfor å lukke døra når det ikke lenger er en hindring.
-- Legge til at hvis heisen er obstructed, og har mottatt ny request, så skal den sende sin nåværende request tilbake til noden, slik at en annen heis fullfører requesten.
-  (Kommer egentlig heisen til å ha noen request hvis den er obstructed? Obstruction stopper ikke heisen fra å åpne døra, så den skal egnetlig kunne cleare sin nåværende request uansett.
-   Og den kommer ikke til å bli satt i IDLE hvis den er obstructed, så den kommer ikke til å motta nye requests.
-   Så dette punktet er kanskje ikke nødvendig. Snakk med Heimvik om dette.)
-- Legge til elevatormusic
-- SKjønne seg på ops greia til Heimert
+- Legge til elevatormusic.
 - Rydde opp i griseriet. Fjerne unødvendige kommentarer og kode.
+- Fjerne unødvendige variabler og funksjoner.
+- Fikse alt av lampegreier.
 */
 
-/*
-Brukseksempel f_getAndSetElevator():
-
-1. Definer opp input/output channels og quit channel:
-c_readElevator := make(chan T_Elevator)
-c_writeElevator := make(chan T_Elevator)
-c_quitGetSetElevator := make(chan bool)
-
-2. Kall den i egen goroutine og les fra c_readElevator:
-func f_GetAndSetElevator(ops, c_readElevator,c_writeElevator,c_quitGetSetElevator)
-oldElevator <- c_readElevator
-
-3. *modifikasjoner på oldElevator* (Mellom her og pkt. 4 må det ikke være noen kall til f_GetElevator() eller f_SetElevator() da dette fører til deadlock)
-newElevator := oldElevator
-
-4. Skriv til global variabel og avslutt getSet goroutine
-c_writeElevator <- newElevator
-c_quitGetSetGlobalQueue <- true
-
-*/
-
-var Elevator T_Elevator
 var C_stop bool
 var C_obstruction bool
 var ID int //temp, spør arbo om flytting
+var C_timerStart chan bool
+var DOOROPENTIME int = 3
 
 func F_RunElevator(ops T_ElevatorOperations, c_requestOut chan T_Request, c_requestIn chan T_Request) {
 
 	Init("localhost:15657") //henter port fra config elno, må smelle på localhost sjæl tror jeg
-	Elevator = Init_Elevator(c_requestIn, c_requestOut)
-
-	Elevator.P_info.State = IDLE
 
 	SetMotorDirection(MD_Down)
 
 	C_stop = false
 	C_obstruction = false
 
-	// c_readElevatorInfo := make(chan T_Elevator)
-	// c_writeElevatorInfo := make(chan T_Elevator)
-	// c_quitGetSet := make(chan bool)
+	c_readElevator := make(chan T_Elevator)
+	c_writeElevator := make(chan T_Elevator)
+	c_quitGetSetElevator := make(chan bool)
 
-	// go node.F_GetAndSetElevator(node.C_nodeOpMsg, c_readElevatorInfo, c_writeElevatorInfo, c_quitGetSet)
+	go F_GetAndSetElevator(ops, c_readElevator, c_writeElevator, c_quitGetSetElevator)
+	//initialize elevator
+	oldElevator := <- c_readElevator
+	oldElevator = Init_Elevator(c_requestIn, c_requestOut)
+	oldElevator.P_info.State = IDLE
+	c_writeElevator <- oldElevator
+	c_quitGetSetElevator <- true
 
+	C_timerStart = make(chan bool)
 	drv_buttons := make(chan ButtonEvent)
 	drv_floors := make(chan int)
 	drv_obstr := make(chan bool)
 	drv_stop := make(chan bool)
 
-	//drv_req := make(chan T_request)
-
 	go PollButtons(drv_buttons)
 	go PollFloorSensor(drv_floors)
 	go PollObstructionSwitch(drv_obstr)
 	go PollStopButton(drv_stop)
-	//go PollNewRequest(c_requestOut)
+	go F_Timer(C_timerStart, c_readElevator, c_writeElevator, c_quitGetSetElevator)
 
 	for {
 		select {
 		case a := <-drv_buttons:
-			F_sendRequest(a, Elevator.C_distributeRequest)
+			oldElevator := F_GetElevator(ops)
+			F_sendRequest(a, oldElevator.C_distributeRequest, oldElevator)
 
 		case a := <-drv_floors:
-			F_fsmFloorArrival(int8(a))
+			oldElevator := <- c_readElevator
+			newElevator := F_fsmFloorArrival(int8(a), oldElevator)
+			c_writeElevator <- newElevator
+			c_quitGetSetElevator <- true
 
-		case a := <-drv_obstr: //tipper dette er nok til å kun teste funksjonalitet
+		case a := <-drv_obstr:
 			C_obstruction = a
-			F_fsmObstructionSwitch(a)
-			//legg til F_fsmObstructionSwitch(a)
-			//den skal ikke gjøre noe hvis heisen ikke er i DOOROPEN
-			//hvis heisen er i DOOROPEN, skal den sette heisen i IDLE og lukke døra hvis C_obstruction er false, hvis ikke skal den ikke gjøre noe
 
 		case a := <-drv_stop:
-			C_stop = a //legg til i F_chooseDirection at hvis C_stop er true, så skal den stoppe
-			F_chooseDirection(Elevator)
+			C_stop = a
+			oldElevator := <- c_readElevator
+			newElevator := F_chooseDirection(oldElevator)
+			c_writeElevator <- newElevator
+			c_quitGetSetElevator <- true
 
-		case a := <-Elevator.C_receiveRequest:
-			fmt.Printf("Elevator: Mottok request fra node\n")
-			Elevator.P_serveRequest = &a
-			F_chooseDirection(Elevator)
-			// SetMotorDirection(MotorDirection(Elevator.P_info.Direction))
-
+		//dette er det jeg ringte deg om
+		case a := <- c_requestIn:
+		// case a := <-Elevator.C_receiveRequest:
+			oldElevator := <- c_readElevator
+			newElevator := F_ReceiveRequest(a, oldElevator)
+			F_chooseDirection(newElevator)
+			c_writeElevator <- newElevator
+			c_quitGetSetElevator <- true
 		}
 	}
 }
